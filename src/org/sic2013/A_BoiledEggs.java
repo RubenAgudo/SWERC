e700a823533649cfa0e447b0060cf0a5fb25787d
Ruben Agudo Santos <rubenagudo21@hotmail.com>
2013-11-07 18:07:19 +0100

    Finished the problem, fixed all bugs

--- src/org/sic2013/A_BoiledEggs.java
+++ src/org/sic2013/A_BoiledEggs.java
@@ -44,7 +44,7 @@ public class A_BoiledEggs {
 	
 	
 	private static int[] W;
-	private static int[][] val;
+	private static int[][][] val;
 	
 	public static void main(String[] args) {
 		Reader.init(System.in);
@@ -58,14 +58,22 @@ public class A_BoiledEggs {
 				int q = Reader.nextInt();
 				
 				W = new int[n];
-				val = new int[n+1][q+1];
+				val = new int[n + 1][q + 1][p + 1];
+				
+				for(int x = 0; x < n+1; x++) {
+					for(int y = 0; y <q+1; y++) {
+						for(int z = 0; z < p+1; z++) {
+							val[x][y][z] = -1;
+						}
+					}
+				}
+				
 				for(int j = 0;j<n;j++)
 				{
 					W[j] = Reader.nextInt();
 				}
 				
 				int result = knapsack(q, p);
-				
 				System.out.println(result);
 			}
 
@@ -86,47 +94,56 @@ public class A_BoiledEggs {
 	}
 	
 	private static int val(int id, int remW, int iterations) {
-		int result = 0;
 		
 		
-		if(iterations > 0) {
+		if(iterations == 0) {
+			return 0;
+		}
+		
+		if(remW == 0) {
+			return 0;
+		}
+		
+		if(id == W.length) {
+			return 0;
+		}
+		
+		if(W[id] > remW) {
 			
-			if(remW == 0) {
-				return 0;
-			}
-			
-			if(id == W.length) {
-				return 0;
-			}
-			
-			if(W[id] > remW) {
+			if(val[id+1][remW][iterations] == -1) {
 				
-				if(val[id+1][remW] == 0) {
-					val[id][remW] = val(id+1, remW, iterations);
-				} else {
-					val[id][remW] = val[id+1][remW];
-				}
-			}
-			
-			if(W[id] <= remW) {
-//				int x = val(id+1, remW, iterations);
-//				int y = W[id] + val(id+1, remW - W[id], iterations -1);
+				val[id][remW][iterations] = val(id+1, remW, iterations);
 				
-				if(val[id+1][remW] == 0) {
-					val[id][remW] = val(id+1, remW, iterations);
-				}
+			} else {
 				
+				val[id][remW][iterations] = val[id+1][remW][iterations];
 				
-				if(val[id+1][remW - W[id]] == 0) {
-					val[id][remW - W[id]] = W[id] + val(id+1, remW - W[id], iterations -1);
-				} else {
-					iterations--;
-				}
-				return Math.max(val[id][remW], val[id][remW - W[id]]);
 			}
 		}
 		
-		return result;
+		if(W[id] <= remW) {
+//			int x = val(id+1, remW, iterations);
+//			int y = W[id] + val(id+1, remW - W[id], iterations -1);
+			
+			if(val[id + 1][remW][iterations] == -1) {
+				
+				val[id][remW][iterations] = val(id + 1, remW, iterations);
+			
+			}
+			
+			if(val[id + 1][remW - W[id]][iterations-1] == -1) {
+				
+				val[id][remW - W[id]][iterations] = W[id] + val(id + 1, remW - W[id], iterations - 1);
+				
+			} else {
+				iterations--;
+			}
+
+			val[id][remW][iterations] = Math.max(val[id][remW][iterations], val[id][remW - W[id]][iterations]);
+			return val[id][remW][iterations];
+		}
+		
+		return 0;
 	}
 
 }
